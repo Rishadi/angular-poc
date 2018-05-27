44200cac565f56acc04b0479db3d0f6b200ecf08
Rishadi <rishadi2000@gmail.com>
2018-05-27 23:28:04 +1200

    Error handling

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -5,6 +5,7 @@ import { Book} from './book';
 import { BOOKS} from './mock-books';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 
@@ -20,10 +21,25 @@ export class BookService {
     }
  
   getBooks(): Observable<Book[]> {
-    // TODO: send the message _after_ fetching the Books
     return this.http.get<Book[]>(this.booksUrl)
+      .pipe(
+        tap(books => this.log(`fetched books`)),
+        catchError(this.handleError('getBooks', []))
+      );
   }
-
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
   getBook(id: number): Observable<Book> {
     // TODO: send the message _after_ fetching the books
     this.messageService.add('BookService: fetched book id=${id}');
