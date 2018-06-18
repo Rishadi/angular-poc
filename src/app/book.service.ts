a84173132412990d66e39468d4a87f203133c93a
Rishadi Dahanayake <rishadi2000@gmail.com>
2018-06-18 23:59:50 +1200

    changes in books service

---
+++ src/app/book.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Book} from './book';
+import { BOOKS } from './mock-books';
+
+@Injectable()
+
+export class BookService {
+
+  constructor() { }
+
+  getBooks(): Book[] {
+    return BOOKS;
+  }
+}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Book} from '../book';
-import { BOOKS } from '../mock-books';
+//import { BOOKS } from '../mock-books';
+import { BookService } from '../book.service';
+
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent implements OnInit {
-  
-  books= BOOKS;
 
+
+export class BooksComponent implements OnInit {
 
   selectedBook: Book;
-  constructor() { }
+  books: Book[];
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit() {
+    this.getBooks();
   }
  
+  getBooks(): void {
+    this.books = this.bookService.getBooks();
+  }
 
   onSelect(book: Book): void {
-  this.selectedBook = book;
+    this.selectedBook = book;
+  }
 }
- }
